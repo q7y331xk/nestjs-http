5d2af26b82282360adf97a80198281b787b6ba2b
dulee <duckyounglee@oden.kr>
2022-06-22 11:09:56 +0900

    fix

--- src/shared/throw-error-in-catch.ts
+++ src/shared/throw-error-in-catch.ts
@@ -1,7 +1,7 @@
 import { ResponseException } from "../types/http-response.type";
 
 export const catchHandler = (err) => {
-  const errArray = Array.isArray(err) && err.length === 2;
+  const errArray = Array.isArray(err) && err.length <= 2;
   if (errArray) throw new ResponseException(...err);
   else throw new ResponseException(err);
 };
